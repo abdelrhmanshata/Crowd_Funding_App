eb01a142a716f6de861849b9bc7572aa911d016c
sarahyabdou <sarayasserma@gmail.com>
2024-02-22 22:02:02 +0200

    add category list ,last 5 projects, and  show featured projects

--- Crowd_Funding_App/home_app/urls.py
+++ Crowd_Funding_App/home_app/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path(
+        "category/<int:category_id>", views.get_category_projects, name="get_category"
+    ),
 ]
--- Crowd_Funding_App/home_app/views.py
+++ Crowd_Funding_App/home_app/views.py
@@ -1,7 +1,51 @@
+
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from admin_app.models import Category
+from projects_app.models import Projects, Image
+from django.db.models import Q
 
 
 def home(request):
-    return render(request,"home/home.html")
+    cat_menu = Category.objects.all()
+    last_5_projects = Projects.objects.order_by("-id")[:5]
+    featured_projects = Projects.objects.filter(featured=True)[:6]
+
+    allLast_5_Data = []
+    for project in last_5_projects:
+        images = Image.objects.filter(project=project)
+        allLast_5_Data.append({"project": project, "images": images})
+
+    Featured_All_Data = []
+    for project in featured_projects:
+        images = Image.objects.filter(project=project)
+        Featured_All_Data.append({"project": project, "images": images})
+        
+    # projects = Projects.objects.all()
+    
+    context = {
+        "cat_menu": cat_menu,
+        "allLast_5_Data": allLast_5_Data,
+        "Featured_All_Data": Featured_All_Data,
+      
+    }
+    return render(request, "home/home.html", context)
+
+
+def get_category_projects(request, category_id):
+    projects = Projects.objects.filter(category=category_id).all()
+    category = Category.objects.get(id=category_id)
+    all_Projects_Data = []
+    for project in projects:
+        images = Image.objects.filter(project=project)
+        all_Projects_Data.append({"project": project, "images": images})
+    
+    context = {
+        "category": category,
+        "all_Projects_Data": all_Projects_Data,
+    }
+    return render(request, "home/category.html", context)
+
+
+
